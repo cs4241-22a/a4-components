d74aff436c14a78ed3524f272680edfba8f9abb0
jacobchlebowski <jachlebowski@wpi.edu>
2022-09-28 18:06:35 -0400

    title colors

---
+++ public/js/title.jsx
@@ -0,0 +1,58 @@
+'use strict';
+
+
+const e = React.createElement;
+const titleRed = <h2 style={{color: 'red'}}>My Homework Assignments</h2>;
+const titleBlue = <h2 style={{color: 'blue'}}>My Homework Assignments</h2>;
+
+
+
+
+class Title extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {title: false}
+  }
+
+
+  render() {
+  
+  if(this.state.title){
+    return e(
+      'header',
+      {onClick: () => this.setState({title: false})},
+      titleBlue,
+    );
+ }
+ else{
+  return e(
+    'header',
+    {onClick: () => this.setState({title: true})},
+    titleRed,
+  );
+ }
+
+
+
+
+  }
+}
+
+
+const domContainer = document.getElementById('title');
+const root = ReactDOM.createRoot(domContainer);
+root.render(e(Title));
+
+
+// in render
+
+
+// if(this.state.title){
+//   return <h2 style={{color: 'blue'}}>My Homework Assignments</h2>;
+//  }
+
+//  return e(
+//    'header',
+//    {onClick: () => this.setState({title: true})},
+//    title,
+//  );
