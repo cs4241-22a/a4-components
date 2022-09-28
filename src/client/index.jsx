6078bcb9fecf12ec8ea36f4395a767e65d01944a
dtavana <datavana@gmail.com>
2022-09-28 00:23:08 -0400

    Enable strict mode on base component

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -3,4 +3,8 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 
 const root = createRoot(document.getElementById("app"));
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
