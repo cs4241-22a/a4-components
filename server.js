a6f187d5bca407526dfec3489dae29c668cff73f
Dillon McCarthy <dillonmccarthy423@gmail.com>
2022-09-29 15:46:26 -0400

    final change to port

--- server.js
+++ server.js
@@ -11,13 +11,6 @@ const workouts = [
 // For avoiding Heroku $PORT error
 app.set('port', (process.env.PORT || 8080));
 
-app.get('/', function(request, response) {
-    var result = 'App is running'
-    response.send(result);
-}).listen(app.get('port'), function() {
-    console.log('App is running, server is listening on port ', app.get('port'));
-});
-
 app.use( express.json() )
 
 app.use( express.static( 'build' ) )
