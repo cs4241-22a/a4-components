46863f8af0a01a1ffbb43e863ce9ab3c84101ef8
Harry Rubin <harry@vps-42ab2d85.vps.ovh.us>
2022-10-01 19:43:11 +0000

    init

---
+++ src/Table.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+class MyTable extends React.Component {
+
+
+    constructor(props) {
+        super(props)
+
+    }
+
+
+    render() {
+        return (
+            <p>test</p>
+        )
+    }
+}
+
+
+export default MyTable
