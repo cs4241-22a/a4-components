a7a25503dfc6bcc5c333504df42bd710cb96498a
= <=>
2022-10-01 22:31:21 +0000

    Turn App into functional component

--- src/App.js
+++ src/App.js
@@ -1,54 +1,42 @@
-import React from "react";
+import {React, useState} from "react";
 import MyTable from "./Table";
 import Form from "./Form";
-import {Grid} from "@mui/material";
+import { Grid } from "@mui/material";
 
-class App extends React.Component {
+export default function App(props) {
+  const [purchases, setPurchases] = useState([
+    {
+      item_name: "Coke",
+      item_quantity: 5,
+      item_cost: 3.2,
+      payment_method: "cash",
+    },
+  ]);
 
-    constructor(props) {
-        super(props);
-        this.setState({
-            purchases: []
-        })
-    }
+  return (
+    <div>
+      <Grid
+        container
+        spacing={0}
+        direction={"column"}
+        alignItems={"center"}
+        justifyContent={"center"}
+      >
+        <Grid item xs={3}>
+          <Form onSubmit={updatePurchases} />
+        </Grid>
+        <Grid item xs={3}>
+          <MyTable purchases={purchases} />
+        </Grid>
+      </Grid>
+    </div>
+  );
 
-    componentDidMount() {
-        this.setState({
-            purchases: {
-                item_name: "Coke", item_quantity: 5, item_cost: 3.20, payment_method: "cash"
-            }
-        })
-    }
-
-    render() {
-        return (
-            <div>
-                <Grid container spacing={0} direction={"column"} alignItems={"center"} justifyContent={"center"}>
-                    <Grid item xs={3}>
-                        <Form onSubmit={this.updatePurchases}/>
-                    </Grid>
-                    <Grid item xs={3}>
-                        <MyTable purchases={this.props.purchases}/>
-                    </Grid>
-                </Grid>
-            </div>
-        );
-    }
-
-    updatePurchases() {
-        fetch("/data").then(data => data.json)
-            .then(data => {
-                this.setState({purchases: data})
-            })
-    }
-
-    decrease() {
-        fetch("/data").then(data => data.json)
-            .then(data => {
-                this.setState({purchases: data})
-            })
-    }
+  function updatePurchases() {
+    fetch("/data")
+      .then((data) => data.json)
+      .then((data) => {
+        this.setPurchases(data);
+      });
+  }
 }
-
-
-export default App;
