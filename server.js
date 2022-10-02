c4f20b377abed3b609dd304936f6abe51d9f1869
fmcshan <fmcshan@wpi.edu>
2022-10-01 23:04:20 -0400

    implement adding and deleting

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 const express  = require( 'express' ),
       app      = express()
 
-const todos = [
-  { name:'buy groceries', completed:false }
-]
+const todos = []
 
 app.use( express.json() )
 
 // this will most likely be 'build' or 'public'
 app.use( express.static( 'dist' ) )
+app.use( express.static( 'src' ) )
 app.get('/', function (req, res) {
     res.sendFile(__dirname + "/dist/index.html")
 })
@@ -16,15 +15,24 @@ app.get('/', function (req, res) {
 app.get( '/read', ( req, res ) => res.json( todos ) )
 
 app.post( '/add', ( req,res ) => {
+  if (req.body.priority == 'Yes') {
+    req.body.dueDate = '1 day'
+  } else {
+    req.body.dueDate = '2 days'
+  }
   todos.push( req.body )
+  console.log(req.body)
   res.json( todos )
 })
 
-app.post( '/change', function( req,res ) {
-  const idx = todos.findIndex( v => v.name === req.body.name )
-  todos[ idx ].completed = req.body.completed
-  
-  res.sendStatus( 200 )
+app.post( '/delete', ( req,res ) => {
+  todos.forEach( (item, i) => {
+    if (item.item == req.body.item && item.date == req.body.date
+      && item.priority == req.body.priority && item.dueDate == req.body.dueDate) {
+      todos.splice(i, 1)
+    }
+  })
+  res.json(todos)
 })
 
 app.listen( 8080 )
--- src/App.js
+++ src/App.js
@@ -2,15 +2,109 @@
 import React from "react";
 import { hot } from 'react-hot-loader/root';
 
-class App extends React.Component {
+let i = 0
+let originalHTML = ''
+let id = 0
+
+class Todo extends React.Component {
   render() {
-    const { name } = this.props;
+    return <tr>
+      <td>{this.props.item}</td>
+      <td>{this.props.date}</td>
+      <td>{this.props.priority}</td>
+      <td>{this.props.dueDate}</td>
+      <td><button className="tableButtons" onClick={e => this.delete(e)}>Delete</button></td>
+    </tr>
+  }
+
+  delete(e) {
+    this.props.onclick( this.props.item, this.props.date, this.props.priority, this.props.dueDate )
+  }
+}
+
+class App extends React.Component {
+  constructor( props ) {
+    super( props )
+    // initialize our state
+    this.state = { todos:[] }
+    this.load()
+  }
+
+  // load in our data from the server
+  load() {
+    fetch( '/read', { method:'get', 'no-cors':true })
+      .then( response => response.json() )
+      .then( json => {
+         this.setState({ todos:json }) 
+      })
+  }
+  
+  // add a new todo list item
+  add( evt ) {
+    const item = document.querySelector('#item').value
+    const date = document.querySelector('#date').value
+    let priority = ""
+
+    if (document.querySelector('#priority').checked) {
+      priority = "Yes"
+    } else {
+      priority = "No"
+    }
+
+    fetch( '/add', { 
+      method:'POST',
+      body: JSON.stringify({ item:item, date:date, priority:priority }),
+      headers: { 'Content-Type': 'application/json' }
+    })
+    .then( response => response.json() )
+    .then( json => {
+        // changing state triggers reactive behaviors
+        this.setState({ todos:json }) 
+    })
+  }
+
+  delete = ( item, date, priority, dueDate ) => {
+    fetch( '/delete', {
+      method:'POST',
+      body: JSON.stringify({ item:item, date:date, priority:priority, dueDate:dueDate }),
+      headers: { 'Content-Type': 'application/json' }
+    }) 
+    .then( response => response.json() )
+    .then( json => {
+        // changing state triggers reactive behaviors
+        this.setState({ todos:json }) 
+    })
+  }
+
+  render() {
     return (
-      <>
-        <h1>
-          Hello {name}
-        </h1>
-      </>
+      <div className="App">
+        <h1 className="child">To Do List Generator</h1>
+        <form className="child">
+          <label>Item:</label>
+          <input type="text" id="item"></input>
+          <label>Date Created:</label>
+          <input type="date" id="date"></input>
+          <label>Priority?</label>
+          <input type="checkbox" id="priority" name="priority"></input>
+          <button id="submit_button" onClick={ e => this.add(e)}>Submit</button>
+        </form>
+        <table className="child" id="list_table">
+          <thead>
+            <tr className="bold">
+              <th>Item</th>
+              <th>Creation Date</th>
+              <th>Priority?</th>
+              <th>Due Date</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+          { this.state.todos.map( (todo,i) =>                  
+          <Todo key={i} item={todo.item} date={todo.date} priority={todo.priority} dueDate={todo.dueDate} onclick={ this.delete } /> ) }
+          </tbody>
+        </table>
+      </div>
     );
   }
 }
