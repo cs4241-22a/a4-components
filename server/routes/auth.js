5b593ba55e2415fc804afb5b3e9b8874daafa34d
Alex Bolduc <aebolduc@wpi.edu>
2022-10-02 11:54:17 -0400

    Fix callback url

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -18,7 +18,7 @@ passport.use(
     {
       clientID: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
-      callbackURL: process.env.ENV == "prd" ? "https://baseball.axbolduc.com/login/github/callback" : "http://127.0.0.1:3000/login/github/callback",
+      callbackURL: process.env.ENV == "prd" ? "https://baseball-react.axbolduc.com/login/github/callback" : "http://127.0.0.1:3000/login/github/callback",
     },
     function (accessToken, refreshToken, profile, cb) {
       User.findOne({ githubId: profile.id }, (err, user) => {
