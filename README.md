f992241c222e0be24449fe61f11b67943972137e
Shrawr <Shrawr@users.noreply.github.com>
2022-10-03 13:00:50 -0400

    first changes

--- README.md
+++ README.md
@@ -1,33 +1,30 @@
-Assignment 4 - Components
-===
+# a4-component
 
-Due: October 4th, by 11:59 AM.
+Empty project.
 
-For this assignment you will re-implement the client side portion of *either* A2 or A3 using either React or Svelte components. If you choose A3 you only need to use components for the data display / updating; you can leave your login UI as is.
+## How to run on localhost
 
-[Svelte Tutorial](https://github.com/cs4241-21a/cs4241-21a.github.io/blob/main/using_svelte.md)  
-[React Tutorial](https://github.com/cs4241-21a/cs4241-21a.github.io/blob/main/using_react.md)  
+First install dependencies:
 
-This project can be implemented on any hosting service (Glitch, DigitalOcean, Heroku etc.), however, you must include all files in your GitHub repo so that the course staff can view them.
+```sh
+npm install
+```
 
-Deliverables
----
+To run in dev mode mode:
 
-Do the following to complete this assignment:
+```sh
+npm start
+```
 
-1. Implement your project with the above requirements.
-3. Test your project to make sure that when someone goes to your main page on Glitch/Heroku/etc., it displays correctly.
-4. Ensure that your project has the proper naming scheme `a4-firstname-lastname` so we can find it.
-5. Fork this repository and modify the README to the specifications below. Be sure to add *all* project files.
-6. Create and submit a Pull Request to the original repo. Name the pull request using the following template: `a4-firstname-lastname`.
+Then go to http://localhost:8080
 
-Sample Readme (delete the above when you're ready to submit, and modify the below so with your links and descriptions)
----
+To create a production build:
 
-## Your Web Application Title
+```sh
+npm run build
+```
 
-your hosting link e.g. http://a4-charlieroberts.glitch.me
+## Credits
 
-Include a very brief summary of your project here and what you changed / added to assignment #3. Briefly (3–4 sentences) answer the following question: did the new technology improve or hinder the development experience?
+Made with [createapp.dev](https://createapp.dev/)
 
-Unlike previous assignments, this assignment will be solely graded on whether or not you successfully complete it. Partial credit will be generously given.
---
+++ server.js
@@ -0,0 +1,213 @@
+const express  = require( 'express' ),
+cookie = require('cookie-session'),
+      app      = express()
+
+const todos = [
+  { name:'buy groceries', completed:false }
+]
+
+app.use( express.json() )
+
+app.use( express.static( 'build' ) ) // this will most likely be 'build' or 'public'
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+app.use(cookie({
+    name: 'session',
+    keys: ['keysplease', 'speediskey']
+  }))
+
+//######################## Database ###########################
+
+const mongodb= require('mongodb');
+const uri = "mongodb+srv://shanestevenz:creeper101@cluster0.dsec9fr.mongodb.net/?retryWrites=true&w=majority";
+const client = new mongodb.MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true});
+
+let collection
+
+
+client.connect()
+  .then(() => {
+    return client.db('a3_data').collection('a3_collection')
+  })
+  .then(__collection => {
+    collection = __collection
+    return collection.find({}).toArray()
+  })
+  .then(console.log)
+
+
+
+
+
+
+//######################## Paths ###########################
+
+
+app.get( '/read', ( req, res ) => res.json( todos ) )
+
+app.post( '/add', ( req,res ) => {
+  todos.push( req.body )
+  res.json( todos )
+})
+
+app.post( '/change', function( req,res ) {
+  const idx = todos.findIndex( v => v.name === req.body.name )
+  todos[ idx ].completed = req.body.completed
+  
+  res.sendStatus( 200 )
+})
+
+
+  
+const loginScripts = [{ script: '/js/clientLogin.js' }];
+app.get('/', (req, res) => {
+    console.log("Get the home page!")
+    res.sendFile('src/public/login.html' , { root : __dirname});
+})
+
+app.get('/mainPage', (req, res) => {
+    res.sendFile('src/mainPage.js' , { root : __dirname});
+})
+
+
+app.use((req, res, next) => {
+  if (collection !== null) {
+    next()
+  } else {
+    res.status(503).send()
+  }
+})
+
+
+
+
+app.post('/register', express.json(), (req, res) => {
+
+    collection
+      .find({ username: req.body.username })
+      .toArray()
+      .then(result => {
+        if (result.length >= 1) {
+          console.log(result)
+          response.json({ login: false });
+        } else {
+          //user does not exist, create
+          let newUser = {
+            username: req.body.username,
+            password: req.body.password,
+            entries: []
+          };
+  
+          console.log(`inserting ${newUser}`);
+          collection.insertOne(newUser);
+       
+          req.session.username = req.body.username;
+          req.session.login = true;
+          res.json({ login: true, username: req.body.username, });
+        }
+      });
+  });
+  
+  
+  
+  
+  
+  
+  app.get('/tasks', (req, res) => { //gets all todoes
+    if (collection !== null) {
+      // get array and pass to res.json
+      console.log("sesison username: " + req.session.username)
+      collection.find({username: req.session.username}).toArray().then(result => res.json(result))
+    }
+  })
+  
+  app.post("/logout", express.json(), (request, response) => {
+    if (request.session.login == true) {
+      request.session.username = "";
+      request.session.login = false;
+  
+      response.json({ logout: true });
+    } else {
+      response.json({ logout: false })
+    }
+  });
+  
+  
+  
+  app.post("/login", express.json(), (request, response) => {
+   
+    collection
+      .find({ username: request.body.username, password: request.body.password })
+      .toArray()
+      .then(result => {
+        if (result.length >= 1) {
+  
+          request.session.username = request.body.username;
+          request.session.login = true;
+          console.log("valid login");
+          console.log(request.session.username);
+          response.json({ login: true });
+  
+        } else {
+  
+          console.log(`invalid login for: ${request.body.username, request.body.password}` )
+          response.json({ login: false });
+        }
+      });
+  });
+  
+  
+  app.post("/addTask", express.json(), (request, response) => {
+  
+    console.log("Due date: " + request.body.dueDate)
+  
+      request.body.daysLeft = getDaysLeft (request.body.dueDate )
+  
+      request.body.username = request.session.username
+      collection.insertOne(request.body).then(result => collection.findOne(result.insertedId)).then( findResult => response.json( findResult))
+      console.log("ADD TASK")
+     
+  });
+  
+  
+  app.post("/clearAll", express.json(), (request, response) => {
+   
+    console.log("Clear All: " + request.session.username)
+    
+     collection
+     .deleteMany({ username: request.session.username, password:{$exists:false}}) //dont delete username password combos , password:{$exists:false}
+     .then( result => response.json( result ) )
+      
+  });
+  
+  app.post("/deleteTask", express.json(), (request, response) => {
+   
+    console.log("Delete Task Id;" +  request.body.id )
+    console.log(request.body)
+      collection
+          .deleteOne({ _id:mongodb.ObjectId( request.body.id ) })
+          .then( result => response.json( result ) )
+   
+  });
+  
+  function getDaysLeft(dateDue) {
+    //initialize dates with Date object
+  
+    let currentDate = new Date ()
+    const _dueDate = new Date(dateDue);
+  
+    // calculation for converting a day into milliseconds
+    const oneDay = 1000 * 60 * 60 * 24;
+  
+    // calculation for the time difference between start and last
+    const diffTime = _dueDate.getTime() - currentDate.getTime();
+  
+    // calculation for the days between start and last
+    const diffDays = Math.round(diffTime / oneDay);
+    // return number of days
+    return diffDays;
+  }
+
+
+app.listen( 8080 )
