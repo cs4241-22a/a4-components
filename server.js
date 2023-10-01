aa0d129a87bc924d3fbb9cf644f84d2a3118d6ec
Brandon Vuong <57996169+bdvuong@users.noreply.github.com>
2023-09-30 22:50:36 -0400

    Add cookie and login validation

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-undef */
 import express from "express";
 import ViteExpress from "vite-express";
+import cookieSession from "cookie-session";
 import { MongoClient } from "mongodb";
 import "dotenv/config";
 
@@ -18,28 +19,35 @@ let collection = null;
 
 // Middleware
 app.use(express.json());
+app.use(
+    cookieSession({
+        name: "session",
+        keys: [process.env.SESS_KEY],
+    })
+);
 
+// Requests
 app.post("/login", async (req, res) => {
+    // Find the user within the MongoDB
     const response = await usersCollection.findOne({
         username: req.body.username,
     });
 
     if (response !== null) {
-        // Change cookie to say we're logged in
-        // Change current collection in the server to the logged in user (might want to change this so it is clientside or write it to the cookie)
         if (response.password === req.body.password) {
-            console.log("Login success")
+            // Change cookie to say we're logged in
+            req.session.user = req.body.username;
+            console.log("Login success");
+            // Change current collection in the server to the logged in user (might want to change this so it is clientside or write it to the cookie)
             collection = db.collection(response.collection);
             res.status(200);
-            res.end();  
-        }
-        else {
-            console.log("Incorrect password")
+            res.end();
+        } else {
+            console.log("Incorrect password");
             res.status(401);
         }
     }
-    // Else say user not found and to create a new user. 
-
+    // Else say user not found and to create a new user.
 });
 
 ViteExpress.listen(app, port, () =>
