7b5931debf6b7e5577654c0af2481973dc9d06be
Brandon Vuong <57996169+bdvuong@users.noreply.github.com>
2023-09-30 21:23:56 -0400

    Create Login.jsx

---
+++ src/screens/Login.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+
+export default function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleLogin(e) {
+    e.preventDefault()
+
+    const body = {
+        username: username,
+        password: password
+    }
+
+    fetch("/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({username, password})
+
+    })
+  }
+
+  return (
+    <form>
+      <input
+        type="text"
+        value={username}
+        placeholder="Username"
+        onChange={(e) => {
+          setUsername(e.target.value);
+        }}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={e => handleLogin(e)}>Login</button>
+    </form>
+  );
+}
