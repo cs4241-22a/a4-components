5122bdd71bf4c864b7b48498e98cace9ede17680
Brandon Vuong <57996169+bdvuong@users.noreply.github.com>
2023-09-30 22:51:47 -0400

    Client-side login

--- src/screens/Login.jsx
+++ src/screens/Login.jsx
@@ -1,39 +1,42 @@
 import { useState } from "react";
 
-export default function Login() {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+export default function Login(props) {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
 
-  function handleLogin(e) {
-    e.preventDefault()
+    function handleLogin(e) {
+        e.preventDefault();
 
-    fetch("/login", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({username, password})
-    })
-    
-  }
+        fetch("/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ username, password }),
+        }).then((response) => {
+            if (response.status === 200) {
+                props.setLoggedIn(true);
+            }
+        });
+    }
 
-  return (
-    <form>
-      <input
-        type="text"
-        value={username}
-        placeholder="Username"
-        onChange={(e) => {
-          setUsername(e.target.value);
-        }}
-      />
-      <input
-        type="password"
-        placeholder="Password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <button onClick={e => handleLogin(e)}>Login</button>
-    </form>
-  );
+    return (
+        <form>
+            <input
+                type="text"
+                value={username}
+                placeholder="Username"
+                onChange={(e) => {
+                    setUsername(e.target.value);
+                }}
+            />
+            <input
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+            />
+            <button onClick={(e) => handleLogin(e)}>Login</button>
+        </form>
+    );
 }
